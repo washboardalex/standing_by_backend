a578d89d948c60bca44812faea4e77d427257782
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-12-01 15:49:16 +1000

    untracking dist

--- dist/utils/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.headers = exports.covidApiUrl = void 0;
-exports.covidApiUrl = 'https://api.covid19api.com';
-exports.headers = {
-    'Content-Type': 'application/json',
-    'Accept-Encoding': 'gzip',
-};
-//# sourceMappingURL=constants.js.map
