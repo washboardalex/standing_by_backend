88828963e9a751d071360f0d89fa343301b81738
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-12-07 07:47:08 +1000

    added log writing to file when not in local dev environment

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 // import cors from 'cors';
 import morgan from 'morgan';
 import bcrypt from 'bcryptjs';
+require('dotenv').config();
+import * as fs from 'fs';
 
 import { db } from './db/db';
 import { setFCMToken } from './controllers/setFCMToken';
@@ -13,7 +15,18 @@ import { deleteAlert } from './controllers/alert/deleteAlert';
 
 
 const app = express();
-app.use(morgan('combined'));
+
+if (process.env.ENVIRONMENT === 'dev') {
+    app.use(morgan('dev'));
+} else {
+    app.use(morgan('common', {
+        stream: fs.createWriteStream('./logs/logs.txt', {flags: 'a'})
+    }));
+}
+
+
+
+
 app.use(bodyParser.json());
 
 setAlertUsersJob();
